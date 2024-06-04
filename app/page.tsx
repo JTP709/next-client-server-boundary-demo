e39d8c88071abd388e422f0e9bcc1bfd0b534de3
Jon Prell <jon@coterieinsurance.com>
2024-06-04 15:53:47 -0600

    simplify client boundary example

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import ClientHome from "./ClientHome";
-import SSRHome from "./SSRHome";
+"use client";
+
+import Counter from "./Counter";
+import ServerComponent from "./ServerComponent";
 
 export default function Home() {
   return (
     <main>
-      <ClientHome />
-      {/* <SSRHome /> */}
+      <Counter />
+      <ServerComponent />
     </main>
   );
 }
